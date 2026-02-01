81c0017a50ff958f7d06609c6e722ed7b5a3d055
Camilla Copetti <camilla.copetti@dimension.it>
2026-02-01 09:51:48 +0100

    quick fix

--- frontend/src/lib/spotify/auth.ts
+++ frontend/src/lib/spotify/auth.ts
@@ -14,8 +14,8 @@ const SCOPES = [
   'user-read-currently-playing',
   'streaming',
   'app-remote-control',
-  'playlist-read-public',
-  'playlist-read-private',
+  //'playlist-read-public',
+  //'playlist-read-private',
 ].join(' ');
 
 /**
